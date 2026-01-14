9dbe2f1ef5e8619d987c38b54e8ecd77effb8fea
SaJaD. <164061116+iqtnt@users.noreply.github.com>
2026-01-14 18:33:33 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,32 @@
+document.getElementById("freeBtn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tabId = tabs[0].id;
+    const currentUrl = tabs[0].url;
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        func: () => {
+          return document.documentElement.innerHTML.toLowerCase();
+        }
+      },
+      (results) => {
+        if (chrome.runtime.lastError) {
+          alert("لا يمكن قراءة محتوى الصفحة");
+          return;
+        }
+
+        const pageSource = results[0].result;
+
+        // 🔍 التحقق من وجود medium.com في السورس
+        if (!pageSource.includes("medium.com")) {
+          alert("❌ هذا الموقع غير تابع إلى Medium");
+          return;
+        }
+
+        const newUrl = "https://freedium-mirror.cfd/" + currentUrl;
+        chrome.tabs.create({ url: newUrl });
+      }
+    );
+  });
+});
